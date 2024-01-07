2690cf1051e13da66e8482e579d5faf95c1eebe4
s-husein <shahrukhh442@gmail.com>
2024-01-07 21:07:24 +0500

    -

--- scripts/paths.py
+++ scripts/paths.py
@@ -1,4 +1,4 @@
-FOLDERPATH = '/home/user/fyp/src/rl_tb3/scripts'
 PLOTFOLDER = '/home/user/fyp/src/rl_tb3/plots'
 MODELFOLDER = '/home/user/fyp/src/rl_tb3/models'
 CHECKPOINTFOLDER = '/home/user/fyp/src/rl_tb3/checkpoints'
+STATUSFILE= '/home/user/fyp/src/rl_tb3/status.txt'
--- scripts/utils.py
+++ scripts/utils.py
@@ -1,46 +1,104 @@
-from paths import PLOTFILE, CHECKPOINTFILE, STATUSFILE
-import pickle as pik
+from paths import CHECKPOINTFOLDER, MODELFOLDER, PLOTFOLDER, STATUSFILE
+import torch
 
+class Utils:
+    def __init__(self):
+        self.model = None
+        self.model_file = ''
+        self.plot_file = ''
+        self.shared_net = None
 
-def read_file(path, byte=False):
-    mode = 'r'
-    if byte:
-        mode = 'rb'
-    file = open(path, mode)
-    file.seek(0)
-    info = file.readline()
-    file.close()
-    return info
+    def read_file(self, path):
+        file = open(path, 'r')
+        file.seek(0)
+        info = file.readline()
+        file.close()
+        return info
 
-def write_file(self, path, content):
+    def write_file(self, path, content):
         mode = 'w'
-        if path == PLOTFILE:
+        if path == self.plot_file:
             mode = '+a'
         file = open(path, mode=mode)
         file.write(content)
         file.close()
-    
 
-def create_file(num):
-    path = f'{CHECKPOINTFILE}/checkpoint_{num}.pth'
-    file = open(path, 'w')
-    file.close()
-    return path
-
-def save_status_dict(data_dict):
-    file = open(STATUSFILE, 'wb')
-    pik.dump(data_dict, file)
-    file.close()
-
-def load_status_dict():
-    file = open(STATUSFILE, 'rb')
-    data = pik.load(file=file)
-    file.close()
-    return data
-
-def check_status_file(epochs):
-    if read_file(STATUSFILE, byte=True) == b'':
-        file = open(STATUSFILE, 'wb')
-        save_status_dict({'path': '', 'num':epochs})
+    def create_checkpoint_file(self, num):
+        path = f'{CHECKPOINTFOLDER}/checkpoint_{num}.pth'
+        file = open(path, 'w')
         file.close()
-    else: pass
+        return path
+
+    def check_status_file(self):
+        checkpath = self.read_file(STATUSFILE)
+        if checkpath != '':
+            epoch = self.load_checkpoint(checkpath) + 1
+            file = open(self.plot_file, 'r')
+            lines = file.readlines()
+            file = open(self.plot_file, 'w')
+            file.writelines(lines[:epoch+1])
+            file.close()
+        else:
+            self.write_file(self.plot_file, 'Rewards\n')
+
+    def save_checkpoint(self, epoch, checkpath):
+        file = open(STATUSFILE, 'w')
+        if self.shared_net:
+            checkpoint = {
+                'model_state_dict': self.model.state_dict(),
+                'optim_state_dict': self.optim.state_dict(),
+                'epoch': epoch
+            }
+        else:
+             checkpoint = {
+                'actor_state_dict': self.actor.state_dict(),
+                'act_optim_state_dict': self.act_optim.state_dict(),
+                'critic_state_dict': self.critic.state_dict(),
+                'crit_optim_state_dict': self.crit_optim.state_dict(),
+                'epoch': epoch
+            }
+        file.write(checkpath)
+        file.close()
+        torch.save(checkpoint, checkpath)
+        print('checkpoint saved..')
+    
+    def load_checkpoint(self, checkpath):
+        print('loading checkpoint..')
+        checkpoint = torch.load(checkpath)
+        if self.shared_net:
+            self.model.load_state_dict(checkpoint['model_state_dict'])
+            self.optim.load_state_dict(checkpoint['optim_state_dict'])
+            self.model.train()
+        else:
+            self.actor.load_state_dict(checkpoint['actor_state_dict'])
+            self.critic.load_state_dict(checkpoint['critic_state_dict'])
+            self.act_optim.load_state_dict(checkpoint['act_optim_state_dict'])
+            self.crit_optim.load_state_dict(checkpoint['crit_optim_state_dict'])
+            self.actor.train()
+            self.critic.train()
+        print('checkpoint loaded...')
+        return checkpoint['epoch']
+    
+    def load_model(self):
+        print('loading model...')
+        if self.shared_net:
+            self.model.load_state_dict(torch.load(self.model_file))
+            self.model.eval()
+        else:
+            model = torch.load(self.model_file)
+            self.actor.load_state_dict(model['actor_state_dict'])
+            self.critic.load_state_dict(model['critic_state_dict'])
+            self.actor.eval()
+            self.critic.eval()
+        print('model loaded...')
+
+    def save_model(self):
+        if self.shared_net:
+            model = {
+                'actor_state_dict': self.actor.state_dict(),
+                'critic_state_dict': self.critic.state_dict()
+            }
+            torch.save(model, self.model_file)
+        else:
+            torch.save(self.model.state_dict(), self.model_file)
+        print('model saved...')
