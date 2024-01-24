cb51984d2b0ef47e0e44fbc8b173d753364cbab1
s-husein <shahrukhh442@gmail.com>
2024-01-25 00:23:11 +0500

    -

--- scripts/nets.py
+++ scripts/nets.py
@@ -9,38 +9,20 @@ class Ordinal(nn.Module):
         super().__init__()
 
     def forward(self, x):
-        x = x.clip(min=1e-6)
-        iden = torch.tril(torch.ones(2*x.size()))
-        inv_iden = 1-iden
-        return torch.sum(iden*torch.log(x)+inv_iden*torch.log(1-x), dim=1)
+        x = torch.sigmoid(x)
+        dims = x.dim()
+        iden = torch.tril(torch.ones_like(torch.eye(x.size()[-1])))[np.newaxis, :, :].repeat(x.size()[0], 1, 1)
+        inv_iden = 1 - iden
+        x = x[:, np.newaxis]
+        return torch.softmax(torch.sum(torch.log(iden*(x) + inv_iden*(1-x)), dim=2).squeeze(), dim=dims-1)
 
 
-def make_dnn(env: Env, hid_layers = [64, 64], action_space='disc', net_type='shared'):
-    layers = []
-    inp = np.prod(env.observation_space.shape)
-    layers.append(nn.Linear(inp,hid_layers[0]))
 
-    dim_pairs = zip(hid_layers[:-1], hid_layers[1:])
-    for in_dim, out_dim in list(dim_pairs):
-        layers.append(nn.Linear(in_dim, out_dim))
-
-    if action_space == 'disc':
-        out = env.action_space.n
-    elif action_space == 'cont':
-        out = np.prod(2*len(env.action_space.sample()))
-    
-    net_types = {'actor': out, 'critic': 1, 'shared': out+1}
-    
-    layers.append(nn.Linear(hid_layers[-1], net_types[net_type]))
-
-    return nn.Sequential(*layers)
-
-def make_discretize_dnn(env: Env, hid_layers = [128, 128], act_fn='relu', bins = 7, ordinal=False):
+def make_dnn(env: Env, hid_layers = [64, 64], action_space='disc', net_type='shared', bins=None, act_fn='relu', ordinal=False):
     layers = []
     activation_fun = {'relu': nn.ReLU(), 'softplus':nn.Softplus(), 'tanh':nn.Tanh()}
-
     inp = np.prod(env.observation_space.shape)
-    layers.append(nn.Linear(inp,hid_layers[0]))
+    layers.append(nn.Linear(inp, hid_layers[0]))
     layers.append(activation_fun[act_fn])
 
     dim_pairs = zip(hid_layers[:-1], hid_layers[1:])
@@ -48,12 +30,18 @@ def make_discretize_dnn(env: Env, hid_layers = [128, 128], act_fn='relu', bins =
         layers.append(nn.Linear(in_dim, out_dim))
         layers.append(activation_fun[act_fn])
 
-    action_dims = len(env.action_space.sample())
+    if action_space == 'disc':
+        out = env.action_space.n
+    elif action_space == 'cont':
+        if bins is not None:
+            out = len(env.action_space.sample())*bins
+        else:
+            out = np.prod(2*len(env.action_space.sample()))
 
-    total_bins = bins*action_dims
-
-    layers.append(nn.Linear(hid_layers[-1], total_bins))
-
+    
+    net_types = {'actor': out, 'critic': 1, 'shared': out+1}
+    
+    layers.append(nn.Linear(hid_layers[-1], net_types[net_type]))
     if ordinal:
         layers.append(Ordinal())
 
@@ -65,7 +53,7 @@ env = gym.make('LunarLanderContinuous-v2')
 # state = torch.tensor(env.reset()[0])
 states = []
 
-for i in range(2):
+for i in range(10):
     states.append(torch.tensor(env.reset()[0]))
 
 states = torch.stack(states)
@@ -73,11 +61,13 @@ states = torch.stack(states)
 print(f'states: {states}')
 
 
-actor = make_discretize_dnn(env, bins = 3, ordinal=False, act_fn='relu')
+actor = make_dnn(env, bins = 7, ordinal=False, act_fn='relu', net_type='actor', action_space='cont')
+
+print(actor)
 
 # print(states.dim())
 
-print(actor(states))
+# print(actor(states))
 
 # print(actor(state))
 
--- scripts/test.py
+++ scripts/test.py
@@ -3,40 +3,33 @@ import numpy as np
 
 # logits = torch.tensor([ 0.0550, -0.0512,  0.1334, -0.0857, -0.1452,  0.0177])
 
-logits = torch.sigmoid(torch.tensor([[ 0.0550, -0.0512, 0.0435, -0.0463],
-                       [ 0.0435, -0.0463, 0.0435, -0.0463]]))
+logits = torch.tensor([[0.5458, 0.5005],
+                        [0.5388, 0.4999],
+                        [0.5455, 0.5011],
+                        [0.5450, 0.5040],
+                        [0.5415, 0.4975],
+                        [0.5409, 0.5012],
+                        [0.5451, 0.5008],
+                        [0.5450, 0.5031],
+                        [0.5427, 0.4990],
+                        [0.5472, 0.5004]])
+
+
+# logits = torch.sigmoid(torch.tensor([[ 0.0550, -0.0512, 0.0435, -0.0463],
+#                        [ 0.0435, -0.0463, 0.0435, -0.0463]]))
+
+# logits = torch.sigmoid(logits)
 
 dims = logits.dim()
 
-print(dims)
-# print(logits.size())
-
-iden = torch.tril(torch.ones_like(torch.eye(logits.size()[-1])))[np.newaxis, :, :].repeat(dims, 1, 1)
+iden = torch.tril(torch.ones_like(torch.eye(logits.size()[-1])))[np.newaxis, :, :].repeat(logits.size()[0], 1, 1)
+print(iden)
 inv_iden = 1 - iden
-
 logits = logits[:, np.newaxis]
+print(inv_iden)
 
+print(iden*(logits) + inv_iden*(1-logits))
 
-res = iden*logits
+res = torch.softmax(torch.sum(iden*torch.log(logits) + inv_iden*torch.log(1-logits), dim=2).squeeze(), dim=dims-1)
 
 print(res)
-
-print(torch.sum(res, dim=2).squeeze())
-
-
-# print(torch.sum(iden*logits, dim=1))
-
-# iden = torch.stack(logits.size()[0]* (torch.tril(torch.ones_like(torch.eye(logits.size(logits.dim()-1)))),))
-
-
-# print(iden*logits)
-
-# inv_iden = 1-iden
-
-# print(inv_iden)
-# res = torch.sum(iden*torch.log(logits)+inv_iden*torch.log(1-logits), dim=1)
-# print(logits)
-
-# print(res)
-
-# print(torch.softmax(res, dim=0))
