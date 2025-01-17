af5b28213b3d10af444f734fbdc3d22f08d8b6c6
s-husein <shahrukhh442@gmail.com>
2025-01-17 12:23:20 +0500

    -

--- scripts/train.py
+++ scripts/train.py
@@ -30,30 +30,30 @@ episodes = params['episodes']+1
 
 for ep in range(epoch, episodes):
     done = False
-    try:
-        state = env.reset()[0]
-        ep_reward = 0
-        steps = 0
-        while not done:
-            d_s = (((np.transpose(state[0], (2, 0, 1))/255.0)-0.5)/0.5).astype(np.float32)
-            action = agent.act(d_s)
-            try:
-                next_state, reward, done, info, _ = env.step(action.cpu().detach().numpy())
-                cv.imshow('depth', state[0])
-                cv.waitKey(1)
-            except:
-                except_flag = True
-                break
-            d_ns = (((np.transpose(next_state[0], (2, 0, 1))/255.0)-0.5)/0.5).astype(np.float32)
-            agent.buffer.add_experience(d_s, action, d_ns, reward, done)
-            state = next_state
-            ep_reward += reward
-            steps += 1
-            if steps >= params['max_steps']:
-                break
-    except:
-        ep -= 1
-        continue
+    # try:
+    state = env.reset()[0]
+    ep_reward = 0
+    steps = 0
+    while not done:
+        d_s = (((np.transpose(state[0], (2, 0, 1))/255.0)-0.5)/0.5).astype(np.float32)
+        action = agent.act(d_s)
+        # try:
+        next_state, reward, done, info, _ = env.step(action.cpu().detach().numpy())
+        cv.imshow('depth', state[0])
+        cv.waitKey(1)
+        # except:
+            # except_flag = True
+            # break
+        d_ns = (((np.transpose(next_state[0], (2, 0, 1))/255.0)-0.5)/0.5).astype(np.float32)
+        agent.buffer.add_experience(d_s, action, d_ns, reward, done)
+        state = next_state
+        ep_reward += reward
+        steps += 1
+        if steps >= params['max_steps']:
+            break
+    # except:
+    #     ep -= 1
+    #     continue
 
     print(f'ep. {ep}\t{ep_reward = :.3f}\t{steps = }')
     agent.write_plot_data(ep_reward)
